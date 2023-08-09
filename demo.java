95aabb6c05107c7b682f7b68db6e696bdb3a25cd
surasrinivas18 <surasrinivas1820@gmail.com>
2023-08-09 10:41:10 +0530

    Revert "new commit"
    This reverts commit 91a0fe8e7838f9767cb75bcce4fc6cb32f4a9e8d.

--- demo.java
+++ demo.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, this is new commit");
+        System.out.println("Hello, this is second commit");
     }
 }
